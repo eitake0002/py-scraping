280a8f88ab265b37af87abd8d6a20cb52016e778
eitake0002 <eiki.e.t.d@gmail.com>
2020-05-22 15:44:48 +0900

    get name, price data and csv file

--- tutorial/spiders/ecommerce_spider.py
+++ tutorial/spiders/ecommerce_spider.py
@@ -2,8 +2,8 @@ import scrapy
 from pprint import pprint
 import pandas as pd
 
-class AmazonSpider(scrapy.Spider):
-    name = "amazon"
+class EcommerceSpider(scrapy.Spider):
+    name = "ecommerce"
 
     def start_requests(self):
         urls = [
@@ -13,6 +13,11 @@ class AmazonSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+
+        # Get item name.
         item_names = response.css("div.b-mod-item-vertical").css("div.b-fix-2lines").css("a::text").getall()
-        
-        pd.DataFrame(item_names).to_csv("item_names.csv", index=False)
+        price_list = response.css("div.b-mod-item-vertical").css("div.m-shop-top-text").css("span::text").getall()
+
+        # Write as CSV file.
+        d = {"item_name": item_names, "price": price_list}
+        pd.DataFrame(d).to_csv("item_names.csv", index=False)
